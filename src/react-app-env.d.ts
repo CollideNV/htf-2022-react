ca16c212c227ecfb0b2c70302b6a75b04f4c2ad1
Levent Önal <levent.onal@collide.be>
2022-11-14 12:38:21 +0100

    installation & setup component

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+
+type CustomElement<T> = Partial<T & DOMAttributes<T> & { children: any }>;
+
+namespace JSX {
+  interface IntrinsicElements {
+    ["htf-2022"]: CustomElement<any>;
+  }
+}
